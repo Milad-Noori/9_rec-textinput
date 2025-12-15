af85ce6ad2b88fb66b9bfab57099c8eaed30b39f
milad <seyeadmiladnouri@gmail.com>
2025-12-15 20:23:01 +0330

    Hen

--- main.py
+++ main.py
@@ -72,13 +72,23 @@ cv2.putText(img,text5,(530,300),font5,1,color=(255,180,130),thickness=1)
 
 
 pt13=(40,580)
-pt14=(240,790)
+pt14=(240,770)
 color6= (217,190,150)
 thickness6 = 1
 text6= "chikhen"
 font6 = cv2.FONT_HERSHEY_DUPLEX
 cv2.rectangle(img,pt13,pt14,color6,thickness6)
 cv2.putText(img,text6,(40,580),font6,1,color=(130,100,120),thickness=1)
+
+
+pt15=(280,580)
+pt16=(500,780)
+color7= (110,75,10)
+thickness7 = 1
+text7= "Hen"
+font7 = cv2.FONT_HERSHEY_DUPLEX
+cv2.rectangle(img,pt15,pt16,color7,thickness7)
+cv2.putText(img,text7,(280,580),font4,1,color=(80,78,130),thickness=1)
 
 
 
